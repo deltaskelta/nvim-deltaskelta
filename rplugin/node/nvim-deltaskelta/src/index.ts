c09072fe9f388851006183412ad573781e0d8f86
Jeff Willette <jrwillette88@gmail.com>
2019-01-09 13:48:44 +0900

    Added command to insert date in frontmatter
    added command to insert date at the end of the line
    added prettierrc

--- rplugin/node/nvim-deltaskelta/src/index.ts
+++ rplugin/node/nvim-deltaskelta/src/index.ts
@@ -1,17 +1,33 @@
 import { NvimPlugin } from 'neovim';
 
-const echo = (plugin: NvimPlugin) => {
-  plugin.nvim.outWriteLine(`it is: ${new Date().toDateString()}`);
-};
-
-const onBufEnter = (plugin: NvimPlugin) => {
-  plugin.nvim.outWriteLine('onBufEnter called');
-};
-
 export default (plugin: NvimPlugin) => {
-  plugin.registerAutocmd('BufEnter', () => onBufEnter(plugin), {
+  plugin.registerAutocmd('BufEnter', () => setTimeout(() => plugin.nvim.outWriteLine('YOU ROCK!'), 2000), {
     pattern: '*',
   });
 
-  plugin.registerCommand('EchoTime', () => echo(plugin));
+  plugin.registerAutocmd('BufWritePre', () => updateTime(plugin), { pattern: '*.md,*.mdx', sync: true });
+  plugin.registerCommand('InsertDate', () => insertDate(plugin));
+};
+
+// update time looks for updatedAt in the frontmatter of the md file and updates it to now
+const updateTime = async (p: NvimPlugin) => {
+  const lines = await p.nvim.buffer.getLines();
+
+  if (lines[0].match('---')) {
+    for (const [i, l] of lines.slice(1).entries()) {
+      if (l.match('updatedAt: ')) {
+        await p.nvim.buffer.setLines([`updatedAt: ${new Date().toUTCString()}`], { start: i + 1, end: i + 2 });
+      }
+
+      if (l.match('---')) {
+        break;
+      }
+    }
+  }
+};
+
+// insert date inserts the date at the end of the line
+const insertDate = async (p: NvimPlugin) => {
+  const line = await p.nvim.getLine();
+  await p.nvim.setLine(`${line} ${new Date().toUTCString()}`);
 };
